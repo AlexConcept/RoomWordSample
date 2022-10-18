c0e18fc7c788975c93e2b51c2d42842da7013d90
Oleksandr <alex505osip@gmail.com>
2022-10-18 20:33:32 +0300

    Add observing database changes - Flow

--- app/src/main/java/com/example/roomwordsample/data/WordDao.kt
+++ app/src/main/java/com/example/roomwordsample/data/WordDao.kt
@@ -3,13 +3,14 @@ package com.example.roomwordsample.data
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 import androidx.room.OnConflictStrategy as OnConflictStrategy
 
 class WordDao {
     @Dao
     interface WordDao {
         @Query("SELECT * FROM word_table ORDER BY word ASC")
-        fun getAlphabetizedWords(): List<Word>
+        fun getAlphabetizedWords(): Flow<List<Word>>
 
         @Insert(onConflict = OnConflictStrategy.IGNORE)
         suspend fun insert(word: Word)
