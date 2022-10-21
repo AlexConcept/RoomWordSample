3e9654cda30ae138e27394a682035a84dab34dd5
Oleksandr <alex505osip@gmail.com>
2022-10-21 19:09:38 +0300

    Populate the database in WordRoomDatabase.kt; Create the coroutineScope in WordApplication.kt;

--- app/src/main/java/com/example/roomwordsample/data/WordApplication.kt
+++ app/src/main/java/com/example/roomwordsample/data/WordApplication.kt
@@ -1,8 +1,11 @@
 package com.example.roomwordsample.data
 
 import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class WordApplication : Application() {
-    val database by lazy { WordRoomDatabase.getDatabase(this) }
+    val applicationScope = CoroutineScope(SupervisorJob())
+    val database by lazy { WordRoomDatabase.getDatabase(this, applicationScope) }
     val repository by lazy { WordRepository(database.wordDao()) }
 }
--- app/src/main/java/com/example/roomwordsample/data/WordRoomDatabase.kt
+++ app/src/main/java/com/example/roomwordsample/data/WordRoomDatabase.kt
@@ -4,19 +4,42 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 
 class WordRoomDatabase {
-    @Database(entities = arrayOf(Word::class), version = 1, exportSchema = false)
-    public abstract class WordRoomDatabase : RoomDatabase() {
+    @Database(entities = [Word::class], version = 1, exportSchema = false)
+    abstract class WordRoomDatabase : RoomDatabase() {
         abstract fun wordDao(): WordDao
+    }
+
+    private class WordDatabaseCallback(private val scope: CoroutineScope) : RoomDatabase.Callback() {
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            INSTANCE?.let { database ->
+                scope.launch {
+                    populateDatabase(database.wordDao())
+                }
+            }
+        }
+
+        suspend fun populateDatabase(wordDao: WordDao) {
+            wordDao.deleteAll()
+            var word = Word(1, "Hello")
+            wordDao.insert(word)
+            word = Word(2, "Word")
+            wordDao.insert(word)
+            word = Word(3, "TODO!")
+            wordDao.insert(word)
+        }
     }
 
     companion object {
         @Volatile
         private var INSTANCE: WordRoomDatabase? = null
 
-        fun getDatabase(context: Context): WordRoomDatabase {
+        fun getDatabase(context: Context, scope: CoroutineScope): WordRoomDatabase {
 
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
@@ -25,6 +48,7 @@ class WordRoomDatabase {
                 ).build()
                 INSTANCE = instance
                 instance
+
             }
         }
     }
